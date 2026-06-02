665fcc4f86ad4b51c6c9c7530670093279ce75ba
jessikarl <182993061+jessikarl@users.noreply.github.com>
2026-06-02 11:25:28 +0200

    update structure

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 My App. All rights reserved.</p>
+    </footer>
+  );
+};
--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,5 +1,11 @@
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+
 export const Layout = () => {
   return (
-    <></>
+    <>
+    <Navbar />
+    <Footer />
+    </>
   );
 };
